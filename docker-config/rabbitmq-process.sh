326911076a8880dafd5871a63619d943a0fe1984
gaurav <gaggarwal@in.imshealth.com>
2020-01-18 17:11:06 +0530

    removed scrap files

--- docker-config/rabbitmq-process.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /var/www/html/literature-review/custom_python
-exec python3 lr_process.py start rabbitMQ &
--- docker-files/Dockerfile.java
+++
@@ -1,8 +0,0 @@
-FROM openjdk:8
-MAINTAINER sohan
-
-RUN java -version
-ADD custom_java/boilerpipe-web-service /home/
-WORKDIR /home
-RUN ./mvnw install
-CMD ./mvnw spring-boot:run
